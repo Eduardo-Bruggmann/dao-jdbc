77630d4b337990e0e8e421915c8f73c1674ae0ca
Blazz <143752142+Eduardo-Bruggmann@users.noreply.github.com>
2025-01-29 14:54:35 -0300

    insert implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,9 +1,11 @@
 package model.dao.impl;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -25,8 +27,39 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public void insert(Seller obj) {
-		// TODO Auto-generated method stub
-
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("""
+					INSERT INTO seller
+					(Name, Email, BirthDate, BaseSalary, DepartmentId)
+					VALUES
+					(?, ?, ?, ?, ?)
+			""", Statement.RETURN_GENERATED_KEYS);
+			
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+			
+			int rowsAffected = st.executeUpdate();
+			
+			if(rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				if(rs.next()) {
+					int id = rs.getInt("id");
+					obj.setId(id);
+				}
+				DB.closeResultSet(rs);
+			} else {
+				throw new DbException("Unexpected error! No rows affected!");
+			}
+			
+		} catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
@@ -52,6 +85,7 @@ public class SellerDaoJDBC implements SellerDao {
 					ON seller.DepartmentId = department.Id
 					WHERE seller.id = ?
 			""");
+			
 			st.setInt(1, id);
 			rs = st.executeQuery();
 			if (rs.next()) {
@@ -79,6 +113,7 @@ public class SellerDaoJDBC implements SellerDao {
 					ON seller.DepartmentId = department.Id
 					ORDER BY ID
 			""");
+			
 			rs = st.executeQuery();
 			
 			List<Seller> sellers = new ArrayList<>();
@@ -115,6 +150,7 @@ public class SellerDaoJDBC implements SellerDao {
 					WHERE DepartmentId = ?
 					ORDER BY NAME
 			""");
+			
 			st.setInt(1, department.getId());
 			rs = st.executeQuery();
 			
